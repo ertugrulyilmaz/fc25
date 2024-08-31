7942e0081248a58dcf2c0b70f45fd501e35bc579
Ertugrul Yilmaz <ert.yil@gmail.com>
2024-08-31 15:31:50 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import TournamentBracket from "./TournamentBracket";
+
+const tournamentData = {
+  rounds: [
+    {
+      name: "Round 1",
+      matches: [
+        { teams: ["Caglar", "Ertugrul"], scores: [2, 2], result: "1-2" },
+        { teams: ["Harun", "Cagatay"], scores: [5, 1], result: "2-2" },
+        { teams: ["Diren", "Baris"], scores: [4, 0], result: "4-1" },
+        { teams: ["Levent", "HRT"], scores: [2, 2], result: "3-2" },
+      ],
+    },
+    {
+      name: "Quarterfinals",
+      matches: [
+        { teams: ["Ertugrul", "Huseyin"], scores: [3, 2], result: "3-1" },
+        { teams: ["Diren", "Levent"], scores: [2, 2], result: "2-3" },
+      ],
+    },
+    {
+      name: "Semifinals",
+      matches: [
+        { teams: ["Batu", "Ertugrul"], scores: [4, 0], result: "3-4" },
+        { teams: ["Huseyin", "Levent"], scores: [5, 0], result: "5-1" },
+      ],
+    },
+    {
+      name: "Final",
+      matches: [{ teams: ["Batu", "Huseyin"], scores: [0, 4] }],
+    },
+  ],
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TournamentBracket />
     </div>
   );
 }
